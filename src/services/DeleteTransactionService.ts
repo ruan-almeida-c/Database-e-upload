d838e26fe55a6bec08b443119b67ff9487869837
ruan <ru_almeida@id.uff.br>
2020-08-16 02:58:28 -0300

    :sparkles: challenge finalized

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+import TransectionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransectionRepository);
+
+    const findTransection = await transactionsRepository.findOne(id);
+
+    if (!findTransection) {
+      throw new AppError('Transaction not found', 204);
+    }
+
+    await transactionsRepository.remove(findTransection);
   }
 }
 
